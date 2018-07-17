29f1f3ac895eb2366bb36d694280b5379ae7e061
Phil Kunz <gitlab@philkunz.com>
2018-07-17 15:47:47 +0200

    fix(dependencies): update to latest standards and slim down dependency tree

--- ts/helpers/country.ts
+++ ts/helpers/country.ts
@@ -1,12 +1,13 @@
 import { SevdeskAccount } from '../sevdesk.classes.account';
 
 export const getCountryIdByCountryName = async (sevdeskAccount: SevdeskAccount, countryNameArg: string) => {
-  const response = await sevdeskAccount.request('GET', `/StaticCountry`);
+  const response = await sevdeskAccount.request('GET', `/StaticCountry?limit=1000`);
   const countryArrray = response.objects;
 
   const countryId = countryArrray.find(countryObjectArg => {
     return countryObjectArg.nameEn === countryNameArg
   }).id
+  // console.log(countryId);
   return {
     id: countryId,
     objectName: 'StaticCountry'
